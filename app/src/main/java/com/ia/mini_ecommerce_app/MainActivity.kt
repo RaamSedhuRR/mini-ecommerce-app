75e54cda110cc14cab4f5cb1851bcb999bb22c0c
raam.sedhu <raam.sedhu@kapturecrm.com>
2025-07-10 18:48:37 +0530

    initial 3 commit

--- app/src/main/java/com/ia/mini_ecommerce_app/MainActivity.kt
+++ app/src/main/java/com/ia/mini_ecommerce_app/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MiniEcommerceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Raam Sedhu RR",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
